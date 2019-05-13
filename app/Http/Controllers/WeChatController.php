502a471791bb0b7db32c36fb8cea54717f10a336
十年饮冰 <735284361@qq.com>
2019-05-13 21:21:33 +0800

    测试群发

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -17,39 +17,39 @@ class WeChatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $app = app('wechat.official_account');
-        $app->server->push(function ($message) {
-            switch ($message['MsgType']) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '收到文字消息';
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                case 'file':
-                    return '收到文件消息';
-                // ... 其它消息
-                default:
-                    return '收到其它消息';
-                    break;
-            }
-
-            // ...
-        });
+//        $app->server->push(function ($message) {
+//            switch ($message['MsgType']) {
+//                case 'event':
+//                    return '收到事件消息';
+//                    break;
+//                case 'text':
+//                    return '收到文字消息';
+//                    break;
+//                case 'image':
+//                    return '收到图片消息';
+//                    break;
+//                case 'voice':
+//                    return '收到语音消息';
+//                    break;
+//                case 'video':
+//                    return '收到视频消息';
+//                    break;
+//                case 'location':
+//                    return '收到坐标消息';
+//                    break;
+//                case 'link':
+//                    return '收到链接消息';
+//                    break;
+//                case 'file':
+//                    return '收到文件消息';
+//                // ... 其它消息
+//                default:
+//                    return '收到其它消息';
+//                    break;
+//            }
+//
+//            // ...
+//        });
 
         $app->broadcasting->sendText("大家好！欢迎使用 EasyWeChat。",['ovFDq1VUIzL6ed56AAnOMgAULXW0','ovFDq1arj2lRv5WG-d8OzBb7OZqk']);
 
