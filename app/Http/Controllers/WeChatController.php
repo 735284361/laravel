da906412cb20f85dee522db257fc0f8e1e4479f7
十年饮冰 <735284361@qq.com>
2019-05-15 18:03:26 +0800

    3

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -33,9 +33,10 @@ class WeChatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $this->app->server->push(function ($message) {
+            Log::info(base64_decode($message));
             switch ($message['MsgType']) {
                 case 'event':
-                    return '收到事件消息'.$message['MsgId'];
+                    return '收到事件消息';
                     break;
                 case 'text':
                     return '收到文字消息';
