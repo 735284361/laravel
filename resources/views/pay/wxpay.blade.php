8945bf64c3ff2d4e75e084dc9d2715bb21ca4641
十年饮冰 <735284361@qq.com>
2019-05-16 16:37:29 +0800

    6

--- resources/views/pay/wxpay.blade.php
+++ resources/views/pay/wxpay.blade.php
@@ -5,11 +5,13 @@
         var url = '{{ url("testApi/wx_pay") }}';
         var csrf_token = "{{ csrf_token() }}";
     </script>
+    <script src="{{ asset('js/jquery-1.11.1.min.js') }}"></script>
     <script src="{{ asset('js/wxpay.js') }}"></script>
 @endsection
 
 @section('content')
 <div class="container">
     <p onclick="pay()">支付</p>
+    <img src="" id="qrcode" alt="">
 </div>
 @endsection
